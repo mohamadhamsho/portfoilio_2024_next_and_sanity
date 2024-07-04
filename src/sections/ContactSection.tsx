105434b75ba9a34cdfde4b8444d45cf476b51fcb
mohamadhamsho <nostalgiedamour2013@gmail.com>
2024-07-04 22:01:55 +0300

    final one

--- src/sections/ContactSection.tsx
+++ src/sections/ContactSection.tsx
@@ -6,7 +6,7 @@ import React from "react";
 
 function ContactSection() {
   return (
-    <section id="contact" className="py-20 mt-8">
+    <section id="contact" className="py-20 mt-8 overflow-hidden">
       <div className="relative bg-card p-6 bg-opacity-80 backdrop-blur-md border border-muted flex flex-col items-center gap-y-6 md:gap-y-12 ">
         <Effect variant={"ball"} size={"default"} className="-top-4 -end-4" />
         <SectionTitle title="Get in touch" description="Let's work together" />
